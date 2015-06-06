9d593f4d7ea565d4994207458e3844f1dba81ece
Qi Chen <aseaday@gmail.com>
2015-06-06 14:08:23 +0800

    Update Struct

---
+++ android/src/io/hackathon/www/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.hackathon.www.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.hackathon.www.wwwGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new wwwGame(), config);
+	}
+}
